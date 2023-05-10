4765e4b2dd3153fc1e2a9109b76a3a6f8913e76f
Michał Łeptuch <michal.leptuch@outlook.com>
2023-05-10 14:56:17 +0200

    More content

---
+++ src/StoreLayoutTemplate/Controls/CustomCard.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Numerics;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace StoreLayoutTemplate.Controls
+{
+  public sealed partial class CustomCard : UserControl, INotifyPropertyChanged
+  {
+    public static new readonly DependencyProperty ContentProperty =
+      DependencyProperty.Register("Content", typeof(UIElement), typeof(CustomCard), new PropertyMetadata(null));
+
+    public CustomCard()
+    {
+      InitializeComponent();
+
+      VisualStateManager.GoToState(this, "Normal", true);
+    }
+
+    public new UIElement Content
+    {
+      get => (UIElement)GetValue(ContentProperty);
+      set
+      {
+        SetValue(ContentProperty, value);
+        PropertyChanged?.Invoke(value, new PropertyChangedEventArgs(nameof(Content)));
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void SetPointerNormalState(object sender, PointerRoutedEventArgs e)
+    {
+      VisualStateManager.GoToState(this, "Normal", true);
+      BackgroundGrid.Translation = new Vector3(0, 0, 0);
+    }
+
+    private void SetPointerOverState(object sender, PointerRoutedEventArgs e)
+    {
+      VisualStateManager.GoToState(this, "PointerOver", true);
+      BackgroundGrid.Translation = new Vector3(0, -2, 6);
+    }
+
+    private void SetPointerPressedState(object sender, PointerRoutedEventArgs e)
+    {
+      VisualStateManager.GoToState(this, "Pressed", true);
+      BackgroundGrid.Translation = new Vector3(0, 0, 0);
+    }
+  }
+}
